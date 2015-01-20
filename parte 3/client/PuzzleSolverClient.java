59977230c94608a668456d50fd25a82d70de74f9
Gabriele Pozzan <fanofthepigpen@gmail.com>
2015-01-20 11:37:25 +0100

    Aggiungi script bash e connessione rmi.

--- parte 3/client/PuzzleSolverClient.java
+++ parte 3/client/PuzzleSolverClient.java
@@ -8,6 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.rmi.*;
 
 public class PuzzleSolverClient{
     private static final Charset charset = StandardCharsets.UTF_8;
@@ -16,13 +17,23 @@ public class PuzzleSolverClient{
 	try (BufferedReader reader = Files.newBufferedReader(inputPath, charset)){
 		String line = null;
 		while((line = reader.readLine()) != null){
-		    result += line;
+		    result += line + "\n";
 		}
 	    } catch(IOException e){
 	    System.err.println(e);
 	}
 	return result;
     }
-    public static void main(String[] args){	
+    public static void main(String[] args) throws Exception{
+	if(args.length < 3){
+	    System.out.println("Errore: manca qualche input.");
+	    return;
+	}
+	String inputFile = args[0];
+	String outputFile = args[1];
+	String name = args[2];
+	Path inputPath = Paths.get(inputFile);
+	Path outputPath = Paths.get(outputFile);
+        PuzzleSolverServerIntf server = (PuzzleSolverServerIntf)Naming.lookup("//"+name+":2020/server");
     }
 }
--- parte 3/server/PuzzleSolverServer.java
+++ parte 3/server/PuzzleSolverServer.java
@@ -4,6 +4,7 @@ import common.*;
 import server.engine.*;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.rmi.registry.*;
 
 public class PuzzleSolverServer extends UnicastRemoteObject implements PuzzleSolverServerIntf{
     PuzzleSolverServer() throws RemoteException {}
@@ -13,11 +14,14 @@ public class PuzzleSolverServer extends UnicastRemoteObject implements PuzzleSol
 	return p;
     }
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
 	if(args.length == 0){
 	    System.err.println("Errore: non è stato fornito il nome del server.");
 	    return;
 	}
-	
+	String name = args[0];
+	LocateRegistry.createRegistry(2020);
+	PuzzleSolverServer server = new PuzzleSolverServer();
+	Naming.rebind("//"+name+":2020/server", server);
     }
 }
